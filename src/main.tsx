a79afc0ef46debff6c7d69ed601a604f94c800c3
strawhat-dev <trankn4@gmail.com>
2022-03-19 04:39:26 -0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
+import { App } from '@/App';
 import { applyGlobalCSS } from '@/lib/stitches';
-import { App } from './App';
 
 applyGlobalCSS();
 
